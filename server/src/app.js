3e288720390d4fd03e79ec408425070d82c5e7a3
Subash2622 <subash9938sahoo@gmail.com>
2026-06-30 22:26:12 +0530

    Add Docker deployment stage

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Welcome to DevOps Learning Backend!"
+        message: "Welcome to DevOps Learning Backend testing!"
     });
 });
 
